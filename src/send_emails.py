0f76930eee08396b97969632ee0b8dec98e06f2d
Arnav Dhariya <dhariyaa@uci.edu>
2025-09-10 22:23:12 -0700

    added code that can send emails and verified that it actaually does that

--- src/send_emails.py
+++ src/send_emails.py
@@ -3,3 +3,32 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email.mime.application import MIMEApplication
 import os
+
+import dotenv
+
+
+dotenv.load_dotenv()
+
+EMAIL = os.getenv("EMAIL")
+PASSWORD = os.getenv("PASSWORD")
+
+def send_email(to_email, subject, body_html):
+    msg = MIMEMultipart()
+    msg["From"] = EMAIL
+    msg["To"] = to_email
+    msg["Subject"] = subject
+
+    msg.attach(MIMEText(body_html, "html"))
+
+    try:
+        with smtplib.SMTP("smtp.gmail.com", 587) as server:
+            server.starttls()
+            server.login(EMAIL, PASSWORD)
+            server.send_message(msg)
+    except Exception as e:
+        print(e)
+
+send_email("arnavdhariya24@gmail.com", "Daily Stock Spam" , """
+    <h2>Your Daily Stock News</h2>
+    <p>Here is your first stock newsletter. </p>
+    """)
